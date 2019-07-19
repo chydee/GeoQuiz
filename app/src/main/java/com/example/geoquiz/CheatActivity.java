885d23227e46b26c1620665863b956d2aba4b989
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-19 21:23:08 +0100

    add extra intent to CheatActivity

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
