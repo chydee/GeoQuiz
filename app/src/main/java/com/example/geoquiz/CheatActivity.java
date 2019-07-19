bbd578bfe7adfafd1cbd98a6fc6b41d9eee6b08b
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-19 21:48:19 +0100

    use extra: retrieve value fom the extra

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -3,6 +3,9 @@ package com.example.geoquiz;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +13,16 @@ public class CheatActivity extends AppCompatActivity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
 
+    private TextView mAnswerTextView;
+
+    private boolean mAnswerIsTrue;
+    private Button mShowAnswerButton;
+
     public static Intent newIntent(Context packageContext, boolean answerIsTrue){
-        Intent intent = new Intent(packageContext, MainActivity.class);
+        Intent intent = new Intent(packageContext, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return intent;
     }
-
-    private boolean mAnswerIsTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,18 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
             mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
+            mAnswerTextView = findViewById(R.id.answer_text_view);
+
+            mShowAnswerButton = findViewById(R.id.show_answer_button);
+            mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mAnswerIsTrue){
+                        mAnswerTextView.setText(R.string.true_button);
+                    } else {
+                        mAnswerTextView.setText(R.string.false_button);
+                    }
+                }
+            });
     }
 }
