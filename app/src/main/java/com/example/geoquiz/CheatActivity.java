c80b77f24bda13110ef140aa108addcdb71395e4
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-22 14:43:30 +0100

    launch CheatActivity

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -16,7 +16,7 @@ public class CheatActivity extends AppCompatActivity {
     public static final String TAG = "CheatActivity";
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
-    public static final String EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown";
+    private static final String EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown";
 
     public static final String KEY_INDEX = "index";
 
@@ -40,28 +40,29 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             mAnswerIsTrue = savedInstanceState.getBoolean(KEY_INDEX, false);
             setAnswerShownResult(true);
         }
 
-            mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-            mAnswerTextView = findViewById(R.id.answer_text_view);
+        mAnswerTextView = findViewById(R.id.answer_text_view);
 
-            mShowAnswerButton = findViewById(R.id.show_answer_button);
-            mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (mAnswerIsTrue){
-                        mAnswerTextView.setText(R.string.true_button);
-                    } else {
-                        mAnswerTextView.setText(R.string.false_button);
-                    }
-                    setAnswerShownResult(true);
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
                 }
-            });
+                setAnswerShownResult(true);
+            }
+        });
     }
+
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
