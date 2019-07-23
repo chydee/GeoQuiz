22a2a92ae74d2c053a54ac7b2d0543e77653ee15
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-23 23:17:02 +0100

    saving state

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
-            mIsCheater = savedInstanceState.getBoolean(KEY_INDEX, false);
+            mIsCheater = savedInstanceState.getBoolean("mIsCheater", false);
         }
 
         mTrueButton = findViewById(R.id.true_button);
@@ -161,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.d(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-        savedInstanceState.putBoolean(KEY_INDEX, mIsCheater);
+        savedInstanceState.putBoolean("mIsCheater", mIsCheater);
     }
 
 
